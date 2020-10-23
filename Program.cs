d631f4ed8036085e7be7e86ebdb9d3d7c62083be
Santhosh V <santhosh.v6426@gmail.com>
2020-10-24 00:26:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+*  C# CODING STANDARDS - LINE SPACE
+* NOTE - A SINGLE BLANK LINE SEPARATES THE TWO BLOCKS i.e. USING STATEMENTS, CLASS DECLARATION, etc
+*/
+
+/******************************************************************************
+ *  Compilation:  csc.exe /t:exe /out:PrimeChecker.exe PrimeChecker.cs
+ *  Execution:    PrimeChecker.exe 103
+ *  
+ *  Purpose: Snake and Ladder Game Simulator.
+ *
+ *  @author  Santhosh V
+ *  @version 2019
+ *  @since   2019
+ *
+ ******************************************************************************/
+
+/// Including the requried assemblies in to the program
+using System;
+
+namespace Snake_and_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Simulator.");
+        }
+    }
+}
